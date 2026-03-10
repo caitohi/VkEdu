a45b3a82042cd5cb484b3e5d47825b68467c07a9
caitohi <caitohisaan@gmail.com>
2026-03-11 02:07:10 +0700

    Добавлен явный Intent с передачей текста между Activity

--- app/src/main/java/com/example/vkedu/MainActivity.kt
+++ app/src/main/java/com/example/vkedu/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.vkedu
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.vkedu.ui.theme.VkEduTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             VkEduTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +41,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val textState = remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VkEduTheme {
-        Greeting("Android")
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        OutlinedTextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            label = { Text("Введите текст") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                val text = textState.value
+                if (text.isNotBlank()) {
+                    val intent = Intent(context, SecondActivity::class.java)
+                    intent.putExtra("USER_TEXT", text)
+                    context.startActivity(intent)
+                } else {
+                    Toast.makeText(context, "Введите текст!", Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Открыть вторую Activity")
+        }
     }
 }

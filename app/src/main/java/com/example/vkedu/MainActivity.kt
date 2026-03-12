77b06a5356e69c93061ce11c456cae0e0ff3e330
caitohi <caitohisaan@gmail.com>
2026-03-13 03:37:59 +0700

    Добавлена навигация между экранами

--- app/src/main/java/com/example/vkedu/MainActivity.kt
+++ app/src/main/java/com/example/vkedu/MainActivity.kt
@@ -1,30 +1,14 @@
 package com.example.vkedu
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import com.example.vkedu.presentation.navigation.NavGraph
 import com.example.vkedu.ui.theme.VkEduTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,100 +18,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             VkEduTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   MainScreen(modifier = Modifier.padding(innerPadding))
+                    NavGraph(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val textState = remember { mutableStateOf("") }
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
-    ) {
-        OutlinedTextField(
-            value = textState.value,
-            onValueChange = { textState.value = it },
-            label = { Text("Введите текст") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                val text = textState.value
-                if (text.isNotBlank()) {
-                    val intent = Intent(context, SecondActivity::class.java)
-                    intent.putExtra("USER_TEXT", text)
-                    context.startActivity(intent)
-                } else {
-                    Toast.makeText(context, "Введите текст!", Toast.LENGTH_SHORT).show()
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Открыть вторую Activity")
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Button(
-            onClick = {
-                val text = textState.value
-
-                if (text.isBlank()) {
-                    Toast.makeText(context, "Введите номер", Toast.LENGTH_SHORT).show()
-                    return@Button
-                }
-
-                val digitsOnly = text.replace("\\D".toRegex(), "")
-
-                if (digitsOnly.isEmpty()) {
-                    Toast.makeText(context, "Введите номер", Toast.LENGTH_SHORT).show()
-                    return@Button
-                }
-
-                val finalNumber = "+7${digitsOnly.take(10)}"
-
-                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$finalNumber"))
-                context.startActivity(intent)
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Позвонить другу")
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Button(
-            onClick = {
-                val text = textState.value
-
-                if (text.isBlank()) {
-                    Toast.makeText(context, "Нет текста для отправки", Toast.LENGTH_SHORT).show()
-                    return@Button
-                }
-
-                val intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    type = "text/plain"
-                    putExtra(Intent.EXTRA_TEXT, text)
-                }
-
-                val chooser = Intent.createChooser(intent, "Поделиться через...")
-                context.startActivity(chooser)
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Поделиться текстом")
         }
     }
 }
